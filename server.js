1de58d3295cf777dbebee7b40fdcdc318e2138fa
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2020-06-13 12:41:21 -0500

    Added logger middleware and finished MVP

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const projectRouter = require("./routers/projectRouter")
 const actionRouter = require("./routers/actionRouter")
+const logger = require("./routers/middleware/logger")
 
 const server = express();
 const port = (process.env.PORT || 5000)
 
+server.use(logger);
 server.use(express.json())
 
 server.use("/project", projectRouter)
