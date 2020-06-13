52bb946173411c16ef1839aa115ebedb45688b39
Austin Kelsay <53542748+AustinKelsay@users.noreply.github.com>
2020-06-13 12:18:12 -0500

    Finished projectRouter

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -16,3 +16,68 @@ router.get('/', (req, res) => {
     })
   })
 });
+
+router.get('/:id', (req, res) => {
+    if (!req.params.id){
+        res.status(400).json({
+            message: "Must include id in your request"
+        })
+    }
+    projects
+   .get(req.params.id)
+   .then((project) => {
+     res.status(200).json(project)
+   })
+   .catch((err) => {
+     res.status(500).json({
+       message: "Error retrieving this specific project"
+     })
+   })
+ });
+
+
+
+ router.get('/actions', (req, res) => {
+     projects
+     .getProjectActions(req.body)
+     .then((actions) => {
+         res.status(200).json(actions)
+     })
+     .catch((err) => {
+        res.status(500).json({
+          message: "Error retrieving actions for this project"
+        })
+      })
+ })
+
+ router.post('/', (req, res) => {
+
+    projects
+     .insert(req.body)
+     .then((project) => {
+         res.status(201).json({message: `Success ${project}`})
+     })
+     .catch((err) => {
+         res.status(500).json({message: `Failure ${err}`})
+     })
+ })
+
+ router.put("/:id", (req, res) => {
+    projects
+     .update(req.params.id, req.body)
+     .then((project) => {
+         res.status(200).json({ project })
+     })
+     .catch((err) => {
+         res.status(500).json({err})
+     })
+ })
+
+ router.delete('/:id', (req, res) => {
+    projects
+      .remove(req.params.id)
+      .then((project) => res.status(200).json(project))
+      .catch((err) => res.status(500).json(err));
+  });
+
+module.exports = router;
